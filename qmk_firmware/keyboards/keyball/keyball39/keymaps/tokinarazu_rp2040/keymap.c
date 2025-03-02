2c5c4f7ffe0bf18dac8bf8c6529ea6195c6e0502
tokinarazu <tokinarazu@gmail.com>
2025-03-02 23:00:23 +0900

    keyball が認識しないので qmk version を元に戻す

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
@@ -344,25 +344,21 @@ void keyball_oled_render_ballinfo_custom(void) {
 
 bool isRecording = false;
 // Triggered when you start recording a macro.
-bool dynamic_macro_record_start_user(int8_t direction) {
+void dynamic_macro_record_start_user(int8_t direction) {
     isRecording = true;
-    return true;
 }
 
 // Triggered when you play back a macro.
-bool dynamic_macro_play_user(int8_t direction) {
-  return true;
+void dynamic_macro_play_user(int8_t direction) {
 }
 
 // Triggered on each keypress while recording a macro.
-bool dynamic_macro_record_key_user(int8_t direction, keyrecord_t *record) {
-  return true;
+void dynamic_macro_record_key_user(int8_t direction, keyrecord_t *record) {
 }
 
 // Triggered when the macro recording is stopped.
-bool dynamic_macro_record_end_user(int8_t direction) {
+void dynamic_macro_record_end_user(int8_t direction) {
     isRecording = false;
-    return true;
 }
 #endif
 
