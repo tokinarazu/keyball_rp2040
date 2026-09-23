91433b9b1ed6b3b2d82ea703a8c28941eb52be86
tokinarazu <tokinarazu@gmail.com>
2026-09-23 20:49:47 +0900

    起動時に自動マウスレイヤー（AML）を強制的にONにする

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
@@ -166,6 +166,11 @@ color.h
 */
 // clang-format on
 
+void keyboard_post_init_user() {
+  // 起動時に自動マウスレイヤー（AML）を強制的にONにする
+  keyball_set_auto_mouse(true); 
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
