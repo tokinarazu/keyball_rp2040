1f10d26e7318c04d784c98a6f03cab8bb2abe0e0
tokinarazu <tokinarazu@gmail.com>
2026-09-23 21:04:15 +0900

    起動時にAMLを強制的にONにする メソッド set_auto_mouse_enabled  が存在しないようなので戻す

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu_rp2040/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tokinarazu_rp2040/keymap.c
@@ -158,11 +158,6 @@ color.h
 */
 // clang-format on
 
-void pointing_device_init_user(void) {
-  // 起動時に自動マウスレイヤー（AML）を強制的にONにする
-  set_auto_mouse_enabled(true);
-}
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
